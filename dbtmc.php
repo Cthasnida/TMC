f2e18c7efcd5c5fa69299bda49ddc524af8abe75
Siti Hasnida <sitihasnida11@gmail.com>
2025-06-22 21:20:26 +0800

    update database

--- dbtmc.php
+++ dbtmc.php
@@ -1,13 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root"; // change if needed
-$password = "";     // change if needed
+$username = "root"; 
+$password = "";     
 $dbname = "dbtmc";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
